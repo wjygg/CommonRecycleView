ca797cf973c2049ef87df242e029c95f0530dde4
wangjingyun <1299880641@qq.com>
2017-07-03 22:13:50 +0800

    通用的recycleview

---
+++ app/src/main/java/com/example/wangjingyun/commonrecycleview/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.wangjingyun.commonrecycleview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //http://blog.csdn.net/lmj623565791/article/details/51118836/
+    }
+}
